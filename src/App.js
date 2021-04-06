168ccd04d591fd236fac9d859feac4a2c6f2c1ce
Michael Stedman <michaelstedman81@gmail.com>
2021-04-06 15:35:25 -0500

    Commit
    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Preview from './components/Preview'
+import voLogo from './assets/images/vo-logo.svg'
 
 const AppContainer = styled.div`
-  border: 2px solid lightblue;
+  width: 100%;
+  height: 1458px;
+  // border: 2px solid lightblue;
 `
 
 function App() {
   return (
     <AppContainer>
-      
+        <img src={voLogo} alt={'Vaxx Overflow'} />
+        <Preview />
     </AppContainer>
   )
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PreviewContainer = styled.div`
+    width: 320px;
+    height: 240px;
+    border: 2px solid black;
+`
+
+export default function Preview() {
+    return (
+        <PreviewContainer>
+            <p>Preview</p>
+        </PreviewContainer>
+    )
+}
