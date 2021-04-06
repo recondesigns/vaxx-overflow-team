966c4a9c2e565bdc7466598e034408832fdf2e7e
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-06 18:27:19 -0500

    Commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,26 +3,32 @@ import styled from 'styled-components'
 import stockHeadshot from '../../assets/images/stock-headshot-1x.jpg'
 
 const CardContainer = styled.div`
+    position: relative;
     margin: 0px;
     max-width: 320px;
-    // height: 172px;    height: 172px;
+    height: 172px;
     background: #FEFEFE;
     border: 2px solid #333333;
     box-sizing: border-box;
     border-radius: 4px;
 
     & > div {
-        // position: absolute;
+        position: absolute;
         width: 68px;
         height: 68px;
-        // left: 20px;
-        // top: 20px;
+        left: 20px;
+        top: 20px;
         border-radius: 4px;
         overflow: hidden;
     }
 
     & > h4 {
+        position: absolute;
+        top: 20px;
+        left: 104px;
+        right: 20px;
         margin: 0px;
+        height: 40px;
         font-family: aktiv-grotesk, sans-serif;
         font-weight: 700;
         font-style: normal;
@@ -42,15 +48,54 @@ const CardContainer = styled.div`
         border: 1px dotted lightblue;
     }
 
+    & > .p-top {
+        position: absolute;
+        top: 72px;
+        left: 104px;
+        right: 20px;
+    }
+
+    & > .p-bottom {
+        position: absolute;
+        top: 92px;
+        left: 104px;
+        right: 20px;
+    }
+
     & > a {
+        // width: 64px;
+        // font-family: aktiv-grotesk, sans-serif;
+        // font-weight: 400;
+        // font-style: normal;
+
         font-family: aktiv-grotesk, sans-serif;
-        font-weight: 400;
+        font-weight: 500;
         font-style: normal;
-        font-size: 16px;
+        font-size: 15px;
         line-height: 20px;
         text-align: center;
         letter-spacing: 0.02em;
         color: #3F7F82;
+        text-decoration: none;
+        // border: 1px dotted darkorange;
+    }
+
+    & > .a-left {
+        position: absolute;
+        top: 132px;
+        left: 104px;
+    }
+
+    & > .a-middle {
+        position: absolute;
+        top: 132px;
+        left: 176px;
+    }
+
+    & > .a-right {
+        position: absolute;
+        top: 132px;
+        left: 244px;
     }
 `
 
@@ -63,11 +108,11 @@ export default function Card() {
                 <img src={stockHeadshot} alt={'Stock headshot'} />
             </div>
             <h4>Member name</h4>
-            <p>Member role</p>
-            <p>Member branch</p>
-            <a href={link}>LinkedIn</a>
-            <a href={link}>Resume</a>
-            <a href={link}>Github</a>
+            <p className={'p-top'}>Member role</p>
+            <p className={'p-bottom'}>Member branch</p>
+            <a href={link} className={'a-left'}>LinkedIn</a>
+            <a href={link} className={'a-middle'}>Resume</a>
+            <a href={link} className={'a-right'}>Github</a>
         </CardContainer>
     )
 }
