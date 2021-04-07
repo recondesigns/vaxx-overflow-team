47864a67c98a6ae4b946f447ee48c7d75a0d0ced
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-07 08:36:02 -0500

    Commit

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -14,13 +14,14 @@ const PreviewContainer = styled.div`
 export default function Preview() {
     return (
         <PreviewContainer>
-            <iframe
+            <p>Preview</p>
+            {/* <iframe
                 title={'preview'} 
                 width="100%" 
                 height="100%" 
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FJhhHkIswpe0EzcVcVtzB7A%2FVaxxOverflow%3Fnode-id%3D302%253A2030" 
                 allowFullScreen
-            ></iframe>
+            ></iframe> */}
         </PreviewContainer>
     )
 }
